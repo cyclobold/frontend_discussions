214d24e50396038b6f967aa2ed2487d2225d2616
Olu <theoafactor@gmail.com>
2022-03-24 14:03:39 +0100

    Added new endpoint

--- script.js
+++ script.js
@@ -21,14 +21,42 @@ registrationForm.addEventListener("submit", function(e){
 
     }
 
-
-
-
-
-
-
-
-
-
 })
+
+const getUsersBtn = document.querySelector("#get-users-btn");
+
+getUsersBtn.onclick = function(){
+
+    axios.get("http://localhost:3000/get-users?email=jane@email.com").then((feedback) =>{
+            //
+            users = feedback.data;
+            console.log(feedback);
+
+            tableCode = `<table>
+                            <thead>
+                                <th>First name</th>
+                                <th>Other name</th>
+                                <th>Email</th>
+                            </thead>
+                            <tbody>
+                            `;
+            for(let i = 0; i < users.length; i++){
+
+                tableCode += `<tr>
+                                    <td>${users[i]['firstname']}</td>
+                                    <td>${users[i]['othername']}</td>
+                                    <td>${users[i]['email']}</td>
+                              </tr>`
+
+            }
+
+
+            tableCode += `</tbody></table>`;
+
+            document.querySelector("#users-area").innerHTML = tableCode;
+
+        })    
+
+
+}
 
